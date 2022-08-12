889e450d91992aecef69efcf7f116e48c73570bf
Janos Batik <janosbatik@gmail.com>
2022-08-12 16:22:17 +0200

    added scale.sh

---
+++ Images/scale.sh
@@ -0,0 +1,19 @@
+read -p "Scale all jps to 1280px? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+NEW_WIDTH=1280
+OUTPUT_DIR="scaled_$NEW_WIDTH"
+SUFX="-${NEW_WIDTH}x"
+EXT=".jpg"
+mkdir -p $OUTPUT_DIR
+echo "copying files..."
+for FILE in *$EXT; do
+   cp $FILE "$OUTPUT_DIR/${FILE%.*}$SUFX$EXT"
+done
+echo "done copying"
+echo "converting files"
+cd $OUTPUT_DIR
+for FILE_COPY in *.jpg; do
+   echo "converting $FILE_COPY"
+   magick  $FILE_COPY -scale $NEW_WIDTH $FILE_COPY
+done
+read -n 1 -s -r -p "Press any key to continue"
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # scripts
 Useful scripts
+
+# image
+
+scale.sh resize all .jpg files in a directory (requires [ImageMagick](https://imagemagick.org/index.php) to be installed)

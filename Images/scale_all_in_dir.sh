f3761fc46ef04db9e09b5e25bbcf70ee146563fe
Janos Batik <janosbatik@gmail.com>
2023-03-16 23:59:36 +0100

    can handle files with spaces in namme

--- Images/scale_all_in_dir.sh
+++ Images/scale_all_in_dir.sh
@@ -3,17 +3,17 @@ read -p "Scale all jps to 1280px? (Y/N): " confirm && [[ $confirm == [yY] || $co
 NEW_WIDTH=1280
 OUTPUT_DIR="scaled_$NEW_WIDTH"
 SUFX="-${NEW_WIDTH}x"
-EXT=".jpg"
+EXT=".JPG"
 mkdir -p $OUTPUT_DIR
 echo "copying files..."
 for FILE in *$EXT; do
-   cp $FILE "$OUTPUT_DIR/${FILE%.*}$SUFX$EXT"
+   cp "$FILE" "$OUTPUT_DIR/${FILE%.*}$SUFX$EXT"
 done
 echo "done copying"
 echo "converting files"
 cd $OUTPUT_DIR
-for FILE_COPY in *.jpg; do
+for FILE_COPY in *$EXT; do
    echo "converting $FILE_COPY"
-   magick  $FILE_COPY -scale $NEW_WIDTH $FILE_COPY
+   magick  "$FILE_COPY" -scale "$NEW_WIDTH" "$FILE_COPY"
 done
 read -n 1 -s -r -p "Press any key to continue"
